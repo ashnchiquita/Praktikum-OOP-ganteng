7cd041bd58bbfce9dda044ce8b57ae274240c7fe
dyophine7 <16521248@mahasiswa.itb.ac.id>
2023-02-27 08:33:38 +0700

    add latihan function template

---
+++ latihan/3-function-template/1-MinArray/MinArray.cpp
@@ -0,0 +1,49 @@
+/*  NIM     : 13521129
+    name    : Chiquita Ahsanunnisa
+    Tanggal : 27 Februari 2023
+    Topik   : Latihan Function Template - MinArray
+*/
+
+#include <iostream>
+using namespace std;
+
+/* FUNGSI MIN ARRAY */
+template <class T>
+T MinArray(T arr[], int N)
+{
+    // PREKONDISI: minimal array x terdiri atas 1 elemen
+    T minVal = arr[0];
+
+    for (int i = 1; i < N; i++) {
+        minVal = arr[i] < minVal ? arr[i] : minVal;
+    }
+
+    return minVal;
+}
+
+/* MAIN PROGRAM */
+int main()
+{
+    int intArr[3];
+    float floatArr[3];
+    double doubleArr[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        intArr[i] = i + 2;
+        floatArr[i] = ((float) i + 4) / 3;
+        doubleArr[i] = ((double) i + 5) / 4 * 7;
+    }
+    
+    cout << "Integer: " << MinArray(intArr, 3) << endl;
+    cout << "Float: " << MinArray(floatArr, 3) << endl;
+    cout << "Double: " << MinArray(doubleArr, 3) << endl;
+
+    return 0;
+}
+
+/* Didefinisikan kelas Jam HH:MM:SS.
+Apakah mungkin MinArray digunakan untuk data bertype Jam?
+
+Jawaban: Mungkin, jika method-method yang dibutuhkan (operator <, =) 
+pada fungsi MinArray tersedia pada kelas Jam */
---
+++ latihan/3-function-template/2-PrintArray/PrintArray.cpp
@@ -0,0 +1,62 @@
+/*  NIM     : 13521129
+    name    : Chiquita Ahsanunnisa
+    Tanggal : 27 Februari 2023
+    Topik   : Latihan Function Template - PrintArray
+*/
+
+#include <iostream>
+using namespace std;
+
+/* FUNGSI PRINT ARRAY */
+template <class T>
+void PrintArray(T arr[], int N)
+{
+    if (N == 0)
+    {
+        cout << "Array kosong" << endl;
+    }
+    else
+    {
+        cout << '[' << arr[0];
+        
+        for (int i = 1; i < N; i++)
+        {
+            cout << ", " << arr[i];
+        }
+
+        cout << ']' << endl;
+    }
+}
+
+/* MAIN PROGRAM */
+int main()
+{
+    int intArr[3];
+    float floatArr[3];
+    double doubleArr[3];
+    char charArr[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        intArr[i] = i + 2;
+        floatArr[i] = ((float) i + 4) / 3;
+        doubleArr[i] = ((double) i + 5) / 4 * 7;
+        charArr[i] = i + 100;
+    }
+    
+    cout << "Integer: ";
+    PrintArray(intArr, 3);
+    cout << "Float: ";
+    PrintArray(floatArr, 3);
+    cout << "Double: ";
+    PrintArray(doubleArr, 3);
+    cout << "Char: ";
+    PrintArray(charArr, 3);
+
+    return 0;
+}
+
+/* Bagaimana kalau yang ingin dicetak adalah Point?
+
+Jawaban: Bisa, jika method-method yang dibutuhkan (operator <<) 
+pada fungsi PrintArray tersedia pada kelas Point */
